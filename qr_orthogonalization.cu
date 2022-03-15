9fff593be268f34bbffda6c054e1ed96114c09ea
younik <omar.younis98@gmail.com>
2022-03-15 12:38:50 +0100

    variable size

--- qr_orthogonalization.cu
+++ qr_orthogonalization.cu
@@ -6,73 +6,77 @@
 using semaphore = cuda::std::counting_semaphore<>;
 
 
-template <int N, typename scalar_t> 
-__global__ void reflections(scalar_t *R, scalar_t *vs, int m, semaphore *sems){ //vs still double precision?
+template <int BLOCK_THREADS, typename scalar_t> 
+__global__ void reflections(scalar_t *R, scalar_t *vs, int m, int n, semaphore *sems){ //vs still double precision?
     int tx = threadIdx.x;
     int bx = blockIdx.x;
-    int m_pos = bx * N + tx;
-    typedef cub::BlockReduce<scalar_t, N, cub::BLOCK_REDUCE_RAKING_COMMUTATIVE_ONLY> BlockReduce;
-    __shared__ typename BlockReduce::TempStorage temp_storage;
+    int v_len = n - bx;
+    __shared__ scalar_t *v = new scalar_t[v_len];
+    int m_pos = bx * n + tx;
 
     if(tx == 0)
-        sems[bx * m + bx].acquire(); //warp accessing simultanuosly? not good
+        sems[bx * m + bx].acquire();
     __syncthreads();
 
-    vs[m_pos] = - (tx >= bx) * R[m_pos]; //try vs[m_pos] in shared mem and push in global at the end
+    for(int idx = tx; idx < v_len; idx += BLOCK_THREADS)
+        v[idx] = - R[bx * n + bx + idx];
 
-    scalar_t z_sq = pow(vs[m_pos], 2);
-    scalar_t norm_z_sq = BlockReduce(temp_storage).Sum(z_sq);
-    if(tx == bx)
-        vs[m_pos] -= copysign(sqrt(norm_z_sq), R[m_pos]);
+    scalar_t norm_v = sqrt(dot<BLOCK_THREADS, scalar_t>(v, v, v_len, tx));
+    if(tx == 0) v[0] += copysign(norm_v, v[0]);
 
-    scalar_t v_sq = pow(vs[m_pos], 2); 
-    scalar_t norm_v_sq = BlockReduce(temp_storage).Sum(v_sq);
-    __shared__ scalar_t norm_v;
-    if(tx == 0)
-        norm_v = sqrt(norm_v_sq);
-    __syncthreads();
+    norm_v = sqrt(dot<BLOCK_THREADS, scalar_t>(v, v, v_len, tx));
+    for(int idx = tx; idx < v_len; idx += BLOCK_THREADS)
+        v[idx] /= norm_v;
 
-    vs[m_pos] /= norm_v;
-
-    for(int m = 0; m < m; ++m){ //dynamic parallelsim and avoid this loop?
-        if(m > bx){
+    for(int row = 0; row < m; ++row){ //dynamic parallelsim and avoid this loop?
+        if(row > bx){
             if(tx == 0)
-                sems[bx * m + m].acquire();   
+                sems[bx * m + row].acquire();   
             __syncthreads();
         }     
 
-        scalar_t prod = R[m * N + tx] * vs[m_pos];
-        scalar_t reduce = BlockReduce(temp_storage).Sum(prod);
-        __shared__ scalar_t dot;
-        if(tx == 0)
-           dot = reduce;
-        __syncthreads();
+        scalar_t prod = R[row * n + tx] * vs[m_pos];
+        scalar_t dot = dot<BLOCK_THREADS, scalar_t>(&R[row * n + bx], v, v_len, tx);
+        
+        for(int idx = tx; idx < v_len; idx += BLOCK_THREADS)
+            R[row * n + bx + idx] -= 2.0 * v[idx] * dot;
 
-        R[m * N + tx] -= 2.0 * vs[m_pos] * dot;
-
-        //if(m > bx){
-        __syncthreads();
-        if (tx == 0)
-            sems[(bx + 1) * m + m].release(); //possible also with if(tx==0) .release(N)
-        //}
+        if(row > bx){
+            __syncthreads();
+            if (tx == 0) sems[(bx + 1) * m + row].release();
+        }
     }
+
+    for(int idx = tx; idx < v_len; idx += BLOCK_THREADS)
+        vs[bx * m + bx + idx] = v[idx]; //TODO: vs not matrix, variable row size
 }
 
-template <int N, typename scalar_t> 
-__global__  void Q_loop(scalar_t *Q, scalar_t *vs, int col){
-    int tx = threadIdx.x;
-    int bx = blockIdx.x;
-    typedef cub::BlockReduce<scalar_t, N, cub::BLOCK_REDUCE_RAKING_COMMUTATIVE_ONLY> BlockReduce;
+template <int BLOCK_THREADS, typename scalar_t> 
+__global__  scalar_t dot(scalar_t *a, scalat_t *b, int length, int tx){
+    typedef cub::BlockReduce<scalar_t, BLOCK_THREADS, cub::BLOCK_REDUCE_RAKING_COMMUTATIVE_ONLY> BlockReduce;
     __shared__ typename BlockReduce::TempStorage temp_storage;
 
-    scalar_t prod = vs[col * N + tx] * Q[bx * N + tx];
-    scalar_t reduce = BlockReduce(temp_storage).Sum(prod);
-    __shared__ scalar_t dot;
-    if(tx == 0)
-       dot = reduce;
-    __syncthreads();
+    __shared__ scalar_t dot = 0;
+    for(int idx = tx; idx < length; idx += BLOCK_THREADS){
+        scalar_t prod = a[idx] * b[idx];
+        scalar_t reduce = BlockReduce(temp_storage).Sum(prod); //not all thread calls when length not power of 2, ok?
 
-    Q[bx * N + tx] -= 2.0 * vs[col * N + tx] * dot;
+        if(tx == 0) dot += reduce;
+        __syncthreads();
+    }
+
+    return dot;
+}
+
+template <int BLOCK_THREADS, typename scalar_t> 
+__global__  void Q_loop(scalar_t *Q, scalar_t *vs, int n, int col){
+    int tx = threadIdx.x;
+    int bx = blockIdx.x;
+    
+    scalar_t dot = dot<BLOCK_THREADS, scalar_t>(&vs[col * n], &Q[bx * n], n, tx);
+
+    for(int idx = tx; idx < n; idx += BLOCK_THREADS)
+        Q[bx * n + idx] -= 2.0 * vs[col * n + idx] * dot;
 }
 
 template <typename scalar_t> 
@@ -103,14 +107,14 @@ void dispatched_implementation(torch::Tensor A, torch::Tensor Q, int m, const in
 
     cudaDeviceSynchronize();
 
-    reflections<1024, scalar_t><<<m, n>>>(A.data<scalar_t>(), vs, m, sems);
+    reflections<1024, scalar_t><<<m, n>>>(A.data<scalar_t>(), vs, m, n, sems);
     
     cudaMemset(Q.data<scalar_t>(), 0, m * n * sizeof(scalar_t));
     add_diag<scalar_t><<<1, m>>>(Q.data<scalar_t>(), n, 1);
     cudaDeviceSynchronize();
 
     for(int col = m - 1; col >= 0; --col){
-        Q_loop<1024, scalar_t><<<m, n>>>(Q.data<scalar_t>(), vs, col);
+        Q_loop<1024, scalar_t><<<m, n>>>(Q.data<scalar_t>(), vs, n, col);
         cudaDeviceSynchronize();
     }
 
@@ -119,7 +123,7 @@ void dispatched_implementation(torch::Tensor A, torch::Tensor Q, int m, const in
 }
 
 void qr_orthogonalization_cuda(torch::Tensor A, torch::Tensor Q, int m, int n, float epsilon){
-    AT_DISPATCH_FLOATING_TYPES(A.scalar_type(), "lltm_forward_cuda", ([&] {
+    AT_DISPATCH_FLOATING_TYPES(A.scalar_type(), "qr_orthogonalization_cuda", ([&] {
         dispatched_implementation<scalar_t>(A, Q, m, n, epsilon);
     }));
 }
