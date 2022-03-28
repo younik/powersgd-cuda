e89c8345b61b64ff81cf0f9f31eaa8814ae2eb6a
younik <omar.younis98@gmail.com>
2022-03-28 08:15:51 +0000

    no sems in QLoop

--- qr_orthogonalization.cu
+++ qr_orthogonalization.cu
@@ -30,7 +30,7 @@ __device__  scalar_t dot(scalar_t *a, scalar_t *b, int length, int tx){
 }
 
 template <int BLOCK_THREADS, typename scalar_t> 
-__global__ void reflections(scalar_t *R, scalar_t *vs, int m, int n, semaphore *sems){ //vs still float precision?
+__global__ void reflections(scalar_t *R, scalar_t *vs, int m, int n, semaphore *sems){ //vs always float precision?
     int tx = threadIdx.x;
     int bx = blockIdx.x;
     int vLen = n - bx;
@@ -50,7 +50,7 @@ __global__ void reflections(scalar_t *R, scalar_t *vs, int m, int n, semaphore *
     for(int idx = tx; idx < vLen; idx += BLOCK_THREADS)
         v[idx] /= normV;
 
-    for(int row = bx + 1; row < m; ++row){ //dynamic parallelsim and avoid this loop?
+    for(int row = bx + 1; row < m; ++row){
         if(tx == 0) sems[bx * m + row].acquire();   
         __syncthreads();
         
@@ -65,22 +65,18 @@ __global__ void reflections(scalar_t *R, scalar_t *vs, int m, int n, semaphore *
 }
 
 template <int BLOCK_THREADS, typename scalar_t> 
-__global__  void QLoop(scalar_t *Q, scalar_t *vs, int n, int m, semaphore *sems){
+__global__  void QLoop(scalar_t *Q, scalar_t *vs, int n, int m){
     int tx = threadIdx.x;
     int bx = blockIdx.x;
-    int vIdx = m - blockIdx.y - 1;
-    scalar_t *v = &vs[vIdx * n];
 
-    if(tx==0) sems[(vIdx + 1) * m + bx].acquire();
-    __syncthreads();
+    for(int vIdx = 0; vIdx < m; ++vIdx){
+        scalar_t *v = &vs[vIdx * n];
     
-    scalar_t dotValue = dot<BLOCK_THREADS, scalar_t>(v, &Q[bx * n], n, tx);
+        scalar_t dotValue = dot<BLOCK_THREADS, scalar_t>(v, &Q[bx * n], n, tx);
 
-    for(int idx = tx; idx < n; idx += BLOCK_THREADS)
-        Q[bx * n + idx] -= 2.0 * v[idx] * dotValue;
-
-    __syncthreads();
-    if(tx==0) sems[vIdx  * m + bx].release();
+        for(int idx = tx; idx < n; idx += BLOCK_THREADS)
+            Q[bx * n + idx] -= 2.0 * v[idx] * dotValue;
+    }
 }
 
 __global__ 
@@ -89,11 +85,6 @@ void initSems(semaphore *sems, int m){
     int bx = blockIdx.x;
 
     new (&sems[bx * m + tx]) semaphore (bx == 0);
-}
-
-__global__
-void releaseSems(semaphore *sems){
-    sems[threadIdx.x].release();
 }
 
 template <typename scalar_t> 
@@ -109,14 +100,12 @@ void dispatchedImplementation(torch::Tensor A, int m, int n, float epsilon){
     reflections<BLOCK_THREADS, scalar_t><<<m, BLOCK_THREADS>>>(A.data<scalar_t>(), vs.data<scalar_t>(), m, n, sems);
     cudaDeviceSynchronize();
 
-    releaseSems<<<1, m>>>(&sems[m*m]);
     cudaMemset(A.data<scalar_t>(), 0, m * n * sizeof(scalar_t));
     A.fill_diagonal_(1);
 
     cudaDeviceSynchronize();
 
-    dim3 blockDim = dim3(m, m);
-    QLoop<BLOCK_THREADS, scalar_t><<<blockDim, BLOCK_THREADS>>>(A.data<scalar_t>(), vs.data<scalar_t>(), n, m, sems);
+    QLoop<BLOCK_THREADS, scalar_t><<<m, BLOCK_THREADS>>>(A.data<scalar_t>(), vs.data<scalar_t>(), n, m);
     cudaDeviceSynchronize();
 
     cudaFree(sems);
